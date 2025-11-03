59fe72558bf7208e6fbdbc1eb00ab6358517345d
김예안 <kjan0929@naver.com>
2025-11-03 19:11:04 +0900

    test(input): 당첨 번호 입력값 검증 테스트 코드 추가

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -1,5 +1,5 @@
 import { ERROR_MESSAGE, TERMS } from '../src/utils/constants';
-import InputValidator from '../src/utils/Validator';
+import InputValidator from '../src/utils/InputValidator';
 
 describe('입력값 검증 테스트 (로또 구입 금액)', () => {
   it.each([
@@ -17,6 +17,45 @@ describe('입력값 검증 테스트 (로또 구입 금액)', () => {
   ])('%s', (_, input, expectedError) => {
     expect(() =>
       InputValidator.runValidate(TERMS.PURCHASE_AMOUNT, input)
+    ).toThrow(expectedError);
+  });
+});
+
+describe('입력값 검증 테스트 (당첨 번호)', () => {
+  it.each([
+    [
+      '구분자가 쉼표(,)가 아닌 경우',
+      '1,2;3,4,5,6',
+      ERROR_MESSAGE.WINNING_NUMBER_TYPE,
+    ],
+    [
+      '구분자 형식이 잘못된 경우',
+      ',1,2,3,4,5,6',
+      ERROR_MESSAGE.WINNING_NUMBER_TYPE,
+    ],
+    [
+      '숫자가 아닌 문자가 포함된 경우',
+      '1,2,h,4,5,6',
+      ERROR_MESSAGE.WINNING_NUMBER_TYPE,
+    ],
+    [
+      '숫자가 6개가 아닌 경우',
+      '1,2,3,4,5',
+      ERROR_MESSAGE.WINNING_NUMBER_LENGTH,
+    ],
+    [
+      '중복된 숫자가 포함된 경우',
+      '1,2,3,4,5,5',
+      ERROR_MESSAGE.WINNING_NUMBER_DUPLICATE,
+    ],
+    [
+      '1~45 범위 밖의 숫자가 포함된 경우',
+      '1,2,3,4,5,50',
+      ERROR_MESSAGE.WINNING_NUMBER_RANGE,
+    ],
+  ])('%s', (_, input, expectedError) => {
+    expect(() =>
+      InputValidator.runValidate(TERMS.WINNING_NUMBER, input)
     ).toThrow(expectedError);
   });
 });
