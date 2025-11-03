8e29c27220c6ec1c9a129e46d323c8d08f4a616f
김예안 <kjan0929@naver.com>
2025-11-03 19:10:38 +0900

    fix(parse): convertToNumberArray 함수가 파싱 시에 Number('') 값을 0으로 처리하는 버그에 대응하는 예외 처리 추가

--- src/utils/Parser.js
+++ src/utils/Parser.js
@@ -8,6 +8,22 @@ class Parser {
   static getPurchaseCount(purchaseAmount) {
     return Number(purchaseAmount) / LOTTO_RULES.TICKET_PRICE;
   }
+
+  /**
+   * 문자열을 구분자로 분리하여 숫자 배열로 변환
+   * @param {string} input - 입력 문자열
+   * @param {string} seperator - 구분자
+   * @returns
+   */
+  static convertToNumberArray(input, seperator) {
+    return input.split(seperator).map((number) => {
+      const trimmed = number.trim();
+
+      if (trimmed === '') return NaN;
+
+      return Number(trimmed);
+    });
+  }
 }
 
 export default Parser;
