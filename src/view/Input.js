30078836fa8b1796e4a6bef269d887461538ca90
김예안 <kjan0929@naver.com>
2025-11-03 15:21:28 +0900

    feat(input): 입력값 클래스 구현
    올바른 입력값을 받을 때까지 반복해서 입력값을 요청하는 기능 포함

---
+++ src/view/Input.js
@@ -0,0 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputValidator from '../utils/Validator.js';
+
+class Input {
+  /**
+   * 올바른 입력을 받을 때까지 반복해서 입력을 요청
+   */
+  static async readValidInput(key, message) {
+    try {
+      const input = await Console.readLineAsync(message);
+      InputValidator.runValidate(key, input);
+
+      return input;
+    } catch (err) {
+      Console.print(err.message);
+      return Input.readValidInput(key, message);
+    }
+  }
+
+  /**
+   * @param {string} key - 검증 키 (e.g. 'purchaseAmount', 'winningNumber')
+   * @param {string} message - 입력 요청 메시지
+   */
+  static readInputValues(key, message) {
+    return Input.readValidInput(key, message);
+  }
+}
+
+export default Input;
